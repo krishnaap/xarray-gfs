d806563bc658d578797d3fc6bb155ac7dd4f82a3
Krishnakumar AP <krishnaap7@gmail.com>
2021-01-09 17:51:17 +0530

    edits in parameter

--- gfs_read.py
+++ gfs_read.py
@@ -9,4 +9,5 @@ Created on Sat Sep 19 19:03:08 2020
 
 import xarray as xr
 
-ds = xr.open_dataset('/media/MediaCentre/Dataset/gdas1.fnl0p25.2019081300.f00.grib2',engine='cfgrib',backend_kwargs={'filter_by_keys': {'typeOfLevel': 'isobaricInPa'}})
+ds = xr.open_dataset('/media/MediaCentre/Dataset/gdas1.fnl0p25.2019081300.f00.grib2',
+                     engine='cfgrib',backend_kwargs={'filter_by_keys': {'typeOfLevel': 'isobaricInhPa'}})
